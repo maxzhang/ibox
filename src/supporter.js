985ac51155eb497d7160cf1266ddd8680ef8867c
maxzhang <zhangdaiping@gmail.com>
2013-10-29 16:47:41 +0800

    修复supporter校验os正则表达式的问题

--- src/supporter.js
+++ src/supporter.js
@@ -1,9 +1,9 @@
 (function(window) {
     var navigator = window.navigator,
         userAgent = navigator.userAgent,
-        android = userAgent.match(/(Android)[\s\/]+([\d\.]+)/),
-        ios = userAgent.match(/(iPad|iPhone|iPod);[\w\s]+OS\s([\d_\.]+)/),
-        wp = userAgent.match(/(Windows\s+Phone)\s([\d\.]+)/),
+        android = userAgent.match(/(Android)\s([\d\.]+)/i),
+        ios = userAgent.match(/(iPad|iPhone|iPod);[\w\s]+(?:iPhone|)\sOS\s([\d_\.]+)/i),
+        wp = userAgent.match(/(Windows\s+Phone)(?:\sOS)?\s([\d\.]+)/i),
         isWebkit = /WebKit\/[\d.]+/i.test(userAgent),
         isSafari = ios ? (navigator.standalone ? isWebkit : (/Safari/i.test(userAgent) && !/CriOS/i.test(userAgent) && !/MQQBrowser/i.test(userAgent))) : false,
         os = {};
@@ -11,11 +11,16 @@
     if (android) {
         os.android = true;
         os.version = android[2];
+        os.android4 = /^4/.test(os.version);
+        os.android3 = /^3/.test(os.version);
+        os.android2 = /^2/.test(os.version);
     }
     if (ios) {
         os.ios = true;
         os.version = ios[2].replace(/_/g, '.');
         os.ios7 = /^7/.test(os.version);
+        os.ios6 = /^6/.test(os.version);
+        os.ios5 = /^5/.test(os.version);
         if (ios[1] === 'iPad') {
             os.ipad = true;
         } else if (ios[1] === 'iPhone') {
@@ -28,6 +33,7 @@
         os.wp = true;
         os.version = wp[2];
         os.wp8 = /^8/.test(os.version);
+        os.wp7 = /^7/.test(os.version);
     }
 
     window.supporter = {
@@ -35,12 +41,18 @@
          * 移动设备操作系统信息，可能会包含一下属性:
          *
          *  Boolean : android
+         *  Boolean : android4
+         *  Boolean : android2
          *  Boolean : ios
          *  Boolean : ios7
+         *  Boolean : ios6
+         *  Boolean : ios5
          *  Boolean : ipad
          *  Boolean : iphone
-         *  Boolean : iphone5
          *  Boolean : ipod
+         *  Boolean : wp
+         *  Boolean : wp8
+         *  Boolean : wp7
          *  String : version 系统版本号
          *
          */
@@ -50,7 +62,7 @@
          * 是否智能设备
          */
         isSmartDevice: (function() {
-            return os.ios || os.android || os.wp;
+            return !!(os.ios || os.android || os.wp);
         }()),
 
         /**
